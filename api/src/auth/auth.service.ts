5653b425cd3b9ca3ace043790bb0e994ce828e44
Jack Vu <jacknvu98@gmail.com>
2024-09-06 14:56:44 -0400

    finished user can log in story with user look up and password matching

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,7 +1,12 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
+import { LogInDto, SignUpDto } from './auth.controller';
 
 @Injectable()
 export class AuthService {
@@ -20,16 +25,23 @@ export class AuthService {
     return await this.jwtService.signAsync(payload);
   }
 
-  async signUp(signUpDto) {
-    const usernameExists =
-      (await this.usersService.findUserByUsername(signUpDto.username)).length >
-      0;
-    const emailExists =
-      (await this.usersService.findUserByEmail(signUpDto.email)).length > 0;
+  async signUp(signUpDto: SignUpDto) {
+    const usernameExists = (
+      await this.usersService.findUserByUsername(signUpDto.username)
+    )?.username;
+    console.log(usernameExists);
+
+    const emailExists = (
+      await this.usersService.findUserByEmail(signUpDto.email)
+    )?.email;
     if (usernameExists) {
+      // TODO remove comment only use for testing
+
       throw new BadRequestException('Username already exists!');
     }
     if (emailExists) {
+      // TODO remove comment only use for testing
+
       throw new BadRequestException('Email already exists!');
     }
     const hashedPassword = await this.hashPassword(signUpDto.password);
@@ -38,14 +50,27 @@ export class AuthService {
     return await this.createAccessToken(user);
   }
 
-  async logIn(logInDto) {
-    console.log(logInDto);
-    
-    // const user = this.usersService.findUserByUsername(logInDto.username)
-    // if (!user) {
-    //   throw new BadRequestException('Username already exists!');
-    // }
-    return "fake token";
+  async verifyPassword(enteredPassword: string, existingPassword: string) {
+    return bcrypt.compare(enteredPassword, existingPassword);
   }
 
+  async logIn(logInDto: LogInDto) {
+    console.log(logInDto);
+    const user = await this.usersService.findUserByUsername(logInDto.username);
+    if (!user) {
+      // TODO remove comment only use for testing
+
+      throw new UnauthorizedException("user doesn't exist");
+    }
+    const passwordsMatch = await this.verifyPassword(
+      logInDto.password,
+      user.password,
+    );
+    if (!passwordsMatch) {
+      // TODO remove comment only use for testing
+      throw new UnauthorizedException('incorrect password');
+    }
+
+    return await this.createAccessToken(user);
+  }
 }
